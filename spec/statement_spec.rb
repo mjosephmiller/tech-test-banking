642c0ba8d92fbc279e570e4949388d1cbc945b62
Michael <mjosephmiller.uk@gmail.com>
2016-09-19 15:42:34 +0100

    adds a test for history variable in statement

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,7 +2,9 @@ require 'account'
 
 
 describe Statement do
+
   subject { described_class.new }
+  let(:account) { Account.new(subject) }
 
   it 'initializes with an empty history' do
     expect(subject.history).to be_empty
@@ -12,6 +14,11 @@ describe Statement do
     expect(subject.print_statement).to eq([])
     expect(STDOUT).to receive(:puts).with("Date || Credit || Debit || Balance ")
     subject.print_statement
+  end
+
+  it 'increases the history as transcations in the account are made' do
+    account.credit(300, subject)
+    expect(subject.history).to eq([{"date"=>"2016-09-19", "credit"=>300, "debit"=>0, "balance"=>300}])
   end
 
 end
